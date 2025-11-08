98213d5910444684a0c779bb0b14c9f7d131a6df
iamthesvn <nitishsrivarun@gmail.com>
2025-11-08 13:51:57 +0530

    added formatted print example

---
+++ Hello-World/_1_2_formatted_print/target/debug/_1_2_formatted_print.d
@@ -0,0 +1,1 @@
+/Users/thesvn/rust-everyday/Hello-World/_1_2_formatted_print/target/debug/_1_2_formatted_print: /Users/thesvn/rust-everyday/Hello-World/_1_2_formatted_print/src/main.rs
---
+++ Hello-World/_1_2_formatted_print/target/debug/deps/_1_2_formatted_print-b8cc9f05180cb9c2.d
@@ -0,0 +1,5 @@
+/Users/thesvn/rust-everyday/Hello-World/_1_2_formatted_print/target/debug/deps/_1_2_formatted_print-b8cc9f05180cb9c2: src/main.rs
+
+/Users/thesvn/rust-everyday/Hello-World/_1_2_formatted_print/target/debug/deps/_1_2_formatted_print-b8cc9f05180cb9c2.d: src/main.rs
+
+src/main.rs:
