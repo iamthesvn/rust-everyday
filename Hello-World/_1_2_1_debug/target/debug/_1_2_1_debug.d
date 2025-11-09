4c8aaabd9eda5f8c7de5fbbc28ae2440dd165730
iamthesvn <nitishsrivarun@gmail.com>
2025-11-09 11:45:44 +0530

    added debug excercise

---
+++ Hello-World/_1_2_1_debug/target/debug/_1_2_1_debug.d
@@ -0,0 +1,1 @@
+/Users/thesvn/rust-everyday/Hello-World/_1_2_1_debug/target/debug/_1_2_1_debug: /Users/thesvn/rust-everyday/Hello-World/_1_2_1_debug/src/main.rs
---
+++ Hello-World/_1_2_1_debug/target/debug/deps/_1_2_1_debug-a55d2e17fdd22646.d
@@ -0,0 +1,5 @@
+/Users/thesvn/rust-everyday/Hello-World/_1_2_1_debug/target/debug/deps/_1_2_1_debug-a55d2e17fdd22646: src/main.rs
+
+/Users/thesvn/rust-everyday/Hello-World/_1_2_1_debug/target/debug/deps/_1_2_1_debug-a55d2e17fdd22646.d: src/main.rs
+
+src/main.rs:
