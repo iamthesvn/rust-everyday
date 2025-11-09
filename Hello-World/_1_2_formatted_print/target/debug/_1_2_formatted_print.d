454c6483298b1267f693083d1154465bd6373cef
iamthesvn <nitishsrivarun@gmail.com>
2025-11-09 11:49:43 +0530

    cleaned target

--- Hello-World/_1_2_formatted_print/target/debug/_1_2_formatted_print.d
+++
@@ -1,1 +0,0 @@
-/Users/thesvn/rust-everyday/Hello-World/_1_2_formatted_print/target/debug/_1_2_formatted_print: /Users/thesvn/rust-everyday/Hello-World/_1_2_formatted_print/src/main.rs
--- Hello-World/_1_2_formatted_print/target/debug/deps/_1_2_formatted_print-b8cc9f05180cb9c2.d
+++
@@ -1,5 +0,0 @@
-/Users/thesvn/rust-everyday/Hello-World/_1_2_formatted_print/target/debug/deps/_1_2_formatted_print-b8cc9f05180cb9c2: src/main.rs
-
-/Users/thesvn/rust-everyday/Hello-World/_1_2_formatted_print/target/debug/deps/_1_2_formatted_print-b8cc9f05180cb9c2.d: src/main.rs
-
-src/main.rs:
