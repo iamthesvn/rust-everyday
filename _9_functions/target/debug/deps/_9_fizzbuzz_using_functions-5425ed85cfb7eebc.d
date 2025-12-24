71d627274c61f705f47c776ac50e3b1647f88995
iamthesvn <nitishsrivarun@gmail.com>
2025-12-24 23:51:09 +0530

    added fizzbuzz using functions exercise

---
+++ _9_functions/target/debug/deps/_9_fizzbuzz_using_functions-5425ed85cfb7eebc.d
@@ -0,0 +1,5 @@
+/Users/thesvn/rust-everyday/_9_functions/target/debug/deps/_9_fizzbuzz_using_functions-5425ed85cfb7eebc.d: src/bin/_9_fizzbuzz_using_functions.rs
+
+/Users/thesvn/rust-everyday/_9_functions/target/debug/deps/_9_fizzbuzz_using_functions-5425ed85cfb7eebc: src/bin/_9_fizzbuzz_using_functions.rs
+
+src/bin/_9_fizzbuzz_using_functions.rs:
