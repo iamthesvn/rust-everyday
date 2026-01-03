603d63871346f2fccaf777185b6aaeba06bb7836
iamthesvn <nitishsrivarun@gmail.com>
2026-01-03 16:09:49 +0530

    added move's impact in closures exercise

---
+++ _9_functions/src/bin/_9_2_1_capturing_move.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // `Vec` has non-copy semantics.
+    let haystack = vec![1, 2, 3];
+
+    let contains = move |needle| haystack.contains(needle);
+
+    println!("{}", contains(&1));
+    println!("{}", contains(&4));
+
+    // println!("There're {} elements in vec", haystack.len());
+    // Uncommenting the above line will result in compile-time error because the borrow checker doesn't allow re-using variable after it has been moved.
+}
